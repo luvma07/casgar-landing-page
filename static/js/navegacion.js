c9e70a6d8270c88a19cc27135a7d6e3559202872
luvma07 <luvma07@gmail.com>
2024-09-02 03:54:57 -0700

    navegacion 1 - panchito

--- static/js/navegacion.js
+++ static/js/navegacion.js
@@ -1,37 +1,67 @@
-window.addEventListener('hashchange', navegacion, false);
-
-function navegacion() {
-    if (location.hash === "#aire-acondicionado") {
-        console.log("Aire acondicionado");
-    }
-    
-    if (location.hash === "#analisis-y-reparacion-de-fallas") {
-        console.log("Analisis y reparación de fallas");
-    }
-    
-    if (location.hash === "#electricidad") {
-        console.log("Electricidad");
-    }
-    
-    if (location.hash === "#equipos-de-combustion") {
-        console.log("Equipos de combustion");
-    }
-    
-    if (location.hash === "#estructuras-metalicas") {
-        console.log("Estructuras metalicas");
-    }
-    
-    if (location.hash === "#mantenimiento-preventivo-y-correctivo") {
-        console.log("Mantenimiento preventivo y correctivo");
-    }
-    
-    if (location.hash === "#soldadura") {
-        console.log("Soldadura");
-    }
-    
-    if (location.hash === "#termografia") {
-        console.log("Termografia Infraroja");
-    }
+function renderTemplate(url) {
+    let mainContent = document.getElementById('mainContent');
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Red no correcta: ' + response.status);
+        }
+        return response.text();
+    })
+    .then(html => {
+        mainContent.innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Hubo un problema con la solicitud fetch:', error);
+    });
 }
+
+
+window.addEventListener('hashchange', navegacion, false);
+function navegacion() {
+    if (location.hash === "#home") {
+        renderTemplate('./templates/home.html');
+    }
+    if (location.hash === "#termografia") {
+        renderTemplate('./templates/termografia_infraroja.html');
+    }
+    if (location.hash === "#equipos-de-combustion") {
+        renderTemplate('./templates/equipos_de_combustion.html');
+    }
+    if (location.hash === "#aire-acondicionado") {
+        renderTemplate('./templates/aire_acondicionado.html');
+    }
+    if (location.hash === "#electricidad") {
+        renderTemplate('./templates/electricidad.html');
+    }
+    if (location.hash === "#soldadura") {
+        renderTemplate('./templates/soldadura.html');
+    }
+    if (location.hash === "#estructuras-metalicas") {        
+        renderTemplate('./templates/estructuras_metalicas.html');
+    }
+    if (location.hash === "#analisis-y-reparacion-de-fallas") {
+        renderTemplate('./templates/analisis_y_reparacion_de_fallas.html');
+    }
+    if (location.hash === "#mantenimiento-preventivo-y-correctivo") {
+        renderTemplate('./templates/analisis_y_reparacion_de_fallas.html');
+    }
+    
+    // Retirar menu
+    let navMovil = document.getElementById("navMovil");
+    navMovil.classList.remove("left-0");
+    navMovil.classList.add("-left-3/4");
+    mainContent.style.backgroundColor = '#FFFFFF';
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hash = window.location.hash;
+    if (hash === '') {
+        window.location.hash = '#home';
+    }
+    navegacion()
+})
+
 
 
